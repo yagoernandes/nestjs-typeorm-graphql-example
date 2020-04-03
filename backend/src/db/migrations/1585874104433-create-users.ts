9d1bea6c1705edca7897db2de7ece2ec1b763b9f
YagoErnandes <yago700@gmail.com>
2020-04-02 21:35:30 -0300

    first

---
+++ backend/src/db/migrations/1585874104433-create-users.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUsers1585874104433 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
